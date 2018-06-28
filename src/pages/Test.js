76ecaa15d069ee7bcbc90a8b7eaa42975b3d2d4f
limitLiu <747638920@qq.com>
2018-06-29 02:50:56 +0800

    use react-native material-design package

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -2,27 +2,30 @@ import React from 'react';
 import {
   Text,
   View,
-  TouchableOpacity,
 } from 'react-native';
 import { actions } from "mirrorx";
 import { connect } from "react-redux";
+import { Button } from 'react-native-material-ui';
 
 @connect(state => ({ counter: state.app.get('counter'), str: state.app.get('rand_str') }))
 export default class Test extends React.Component {
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <TouchableOpacity onPress={async () => {
-          // this.setState((state) => ({ test: state.test + 1 }));
-          await actions.app.addClick();
-        }}>
-          <Text>{this.props.str}</Text>
-          <Text>{`${this.props.counter}`}</Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity onPress={this.props.onBack}>
-          <Text>onBack</Text>
-        </TouchableOpacity>
+        <Text>{this.props.str}</Text>
+        <Button
+          onPress={async () => {
+            await actions.app.addClick()
+          }}
+          upperCase={false}
+          primary
+          raised
+          text={`${this.props.counter}`}/>
+
+
+        <Button upperCase={false} onPress={this.props.onBack} text='onBack' raised accent/>
       </View>
     );
   }
